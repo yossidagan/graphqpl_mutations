56515b081fdc7513686b36e0b7b4509465111bc0
yossi dagan <dagangm@gmail.com>
2020-05-09 14:34:05 +0300

    added main graphql route and created schema

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
-
+const graphqlHTTP = require('express-graphql')
 const app = express()
+
+const schema = require('./schema')
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql : true
+}))
 
 const PORT = 4000
 
