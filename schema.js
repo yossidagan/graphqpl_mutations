5efe6b56234baf03027dac7adea1650fed5c5d99
yossi dagan <dagangm@gmail.com>
2020-05-09 17:45:13 +0300

    added mutation - delete user

--- schema.js
+++ schema.js
@@ -63,6 +63,17 @@ const mutation = new GraphQLObjectType({
           })
           .then((res) => res.data)
       }
+    },
+    deleteCustomer: {
+      type: CustomerType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parent, args) {
+        return axios
+          .delete(`http://localhost:3000/customers/${args.id}`)
+          .then((res) => res.data)
+      }
     }
   }
 })
